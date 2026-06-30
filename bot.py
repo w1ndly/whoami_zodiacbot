c3fc5ca0da52c84b5b1c3ef45a2642225d773ad5
w1ndly <angelornis@gmail.com>
2026-07-01 01:38:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ from handlers.help import router as help_router
 from handlers.profile import router as profile_router
 from handlers.clear import router as clear_router
 from handlers.start import router as start_router
-from handlers.callbacks import router as from handlers.callbacks import router as callbacks_router, configure_callbacks
+from handlers.callbacks import router as callbacks_router, configure_callbacks
 
 dp = Dispatcher()
 dp.include_router(feedback_router)
